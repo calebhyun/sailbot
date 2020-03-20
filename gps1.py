5e402e5139aea7a02a86c2d7c73728bd764e4dd8
calebhyun <60834862+calebhyun@users.noreply.github.com>
2020-03-20 06:35:27 +0000

    Create gps1.py

---
+++ gps1.py
@@ -0,0 +1,33 @@
+#! /usr/bin/python
+ 
+from gps import *
+import time
+    
+gpsd = gps(mode=WATCH_ENABLE|WATCH_NEWSTYLE) 
+print('latitude\tlongitude\ttime utc\t\t\taltitude\tepv\tept\tspeed\tclimb') # '\t' = TAB to try and output the data in columns.
+   
+try:
+ 
+ 
+    while True:
+        report = gpsd.next() #
+        if report['class'] == 'TPV':
+            print("TPV")
+            print(getattr(report,'lat',0.0),"\t")
+            print(getattr(report,'lon',0.0),"\t")
+            print(time.time())
+            #print(getattr(report,'time',''),"\t")
+            #print(getattr(report,'alt','nan'),"\t\t")
+            #print(getattr(report,'epv','nan'),"\t")
+            #print(getattr(report,'ept','nan'),"\t")
+            #print(getattr(report,'speed','nan'),"\t")
+            #print(getattr(report,'climb','nan'),"\t")
+        else:
+            print("else")
+            print(getattr(report,'lat',0.0),"\t")
+            print(getattr(report,'lon',0.0),"\t")
+            print(time.time())
+            
+except (KeyboardInterrupt, SystemExit): #when you press ctrl+c
+    print("Done.\nExiting.")
+
