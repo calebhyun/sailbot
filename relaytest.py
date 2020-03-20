4fef31bd1e9a143f1fec66dedab6a69acbd363ec
calebhyun <60834862+calebhyun@users.noreply.github.com>
2020-03-20 07:21:28 +0000

    Create relaytest.py

---
+++ relaytest.py
@@ -0,0 +1,20 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(32, GPIO.OUT)
+GPIO.setup(36, GPIO.OUT)
+
+for x in range(20):
+    print("LEFT")
+    GPIO.output(32, GPIO.HIGH)
+    GPIO.output(36, GPIO.LOW)
+    time.sleep(1)
+    print("STRAIGHT")
+    GPIO.output(32, GPIO.HIGH)
+    GPIO.output(36, GPIO.HIGH)
+    time.sleep(1)    
+    print("RIGHT")
+    GPIO.output(32, GPIO.LOW)
+    GPIO.output(36, GPIO.HIGH)    
+    time.sleep(1)
+GPIO.cleanup()
