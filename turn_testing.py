cdc92dd3ee884b8367b0c71307851d5a919e4699
calebhyun <60834862+calebhyun@users.noreply.github.com>
2020-12-03 06:18:00 +0000

    Create turn_testing.py

--- turn_testing.py
+++ turn_testing.py
@@ -7,20 +7,42 @@ import datetime
 dt = datetime.datetime.today()
 import magnetometer
 import pygame, sys
-import pygame.locals
+from pygame.locals import *
 
 
-'''pygame.init()
+pygame.init()
+
 BLACK = (0,0,0)
-WIDTH = 100
-HEIGHT = 100
-windowSurface = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
+WHITE = (255,255,255)
+GREEN = (0,255,0)
+BLUE = (0,0, 255)
+RED = (255, 0, 0)
 
-windowSurface.fill(BLACK)'''
+SCREENWIDTH = 1000
+SCREENHEIGHT = 600
+WIDTHMIDPOINT = SCREENWIDTH/2
+HEIGHTMIDPOINT = SCREENHEIGHT/2
 
-#Things that are off:
-#gps shifts slightly throughout (creating projected heading change of <20)
-#compass bearing is a bit off (<15)
+windowSurface = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT), 0, 32)
+
+windowSurface.fill(BLACK)
+
+DISPLAYSURF = pygame.display.set_mode(((SCREENWIDTH, SCREENHEIGHT)))
+
+fontObj = pygame.font.SysFont('Times New Roman', 16)
+
+def writeText(text, position, color = WHITE):
+    global fontObj, DISPLAYSURF
+        
+    txtSurf = fontObj.render(text, True, color)
+    txtRect = txtSurf.get_rect()
+    txtRect.topleft = (position)
+
+    DISPLAYSURF.blit(txtSurf, txtRect)
+
+
+#########################################################
+
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(32, GPIO.OUT)
@@ -32,23 +54,20 @@ GPIO.setup(10, GPIO.OUT)
 GPIO.setup(12, GPIO.OUT)
 GPIO.output(38, GPIO.HIGH)
 
-tolerancelat = .00005
-tolerancelong = .00005
+tolerancelat = .00003
+tolerancelong = .00003
 
 def goLeft():
-    print("LEFT")
     GPIO.output(8, GPIO.HIGH)
     
     #GPIO.output(32, GPIO.LOW)
     #GPIO.output(36, GPIO.HIGH)
 def goRight():
-    print("RIGHT")
     GPIO.output(12, GPIO.HIGH)
 
     #GPIO.output(32, GPIO.HIGH)
     #GPIO.output(36, GPIO.LOW)
 def goStraight():
-    print("STRAIGHT")
     GPIO.output(10, GPIO.HIGH)
 
     GPIO.output(32, GPIO.HIGH)
@@ -56,12 +75,15 @@ def goStraight():
 
 def goForwards():
     #GPIO.output(38, GPIO.LOW)
-    print("\n")
+    filler = 0
+
 def stop():
     GPIO.output(38, GPIO.HIGH)
     GPIO.output(10, GPIO.LOW)
     GPIO.output(12, GPIO.LOW)
     GPIO.output(8, GPIO.LOW)
+
+######################################################
 
 gpsd = gps(mode=WATCH_ENABLE|WATCH_NEWSTYLE)
 
@@ -107,14 +129,13 @@ def calculate_initial_compass_bearing(pointA, pointB):
     # from -180° to + 180° which is not what we want for a compass bearing
     # The solution is to normalize the initial bearing as shown below
     initial_bearing = math.degrees(initial_bearing)
-    compass_bearing = (initial_bearing + 360) % 360
 
+    compass_bearing = (initial_bearing +360) % 360
     return compass_bearing
 
 def steering():
-    global used, cur_long, cur_lat
+    global used, cur_long, cur_lat, heading, necessary_change, projected_bearing, position
     if used!=True:
-        print("being used")
         used = True
         report = gpsd.next() #
         if report['class'] == 'TPV':
@@ -123,11 +144,8 @@ def steering():
             
             projected_bearing = calculate_initial_compass_bearing((cur_lat, cur_long), (lat1, long1))
             
-            
-            print(cur_lat, ", ", cur_long,"\t")
             heading = magnetometer.get_heading()
-
-            print("bearing: ", heading, ",", "projected_bearing: ", projected_bearing)
+            print(heading)
             
             necessary_change = projected_bearing - heading
             	
@@ -136,26 +154,29 @@ def steering():
                 
             elif necessary_change >= 180:
                 necessary_change -= 360
-
-            print("necessary change: ", necessary_change)
-            #print("necessarychange changed")
             
             m = abs(necessary_change / 50);
             if necessary_change > 5:
                 goRight()
+                position = "Right"
                 time.sleep(.2)
                 goStraight()
             elif necessary_change < -5:
                 goLeft()
+                position = "Left"
                 time.sleep(.2)
                 goStraight()
             else:
                 goStraight()
-        #print("steering done")
+                position = "Straight"
         used = False
 
-lat1 = 0.00029 #47.34211
-long1 = -0.00019 #-122.32705
+lat1 = 0.0 #47.34211
+long1 = 0.0 #-122.32705
+heading = 0.0
+projected_bearing = 0.0
+necessary_change = 0.0
+position = "Straight"
 
 report = gpsd.next()
 while report['class'] != 'TPV':
@@ -174,31 +195,65 @@ num_of_coordinates = len(coordinates_array)
 
 for y in range(0, num_of_coordinates):
     both_coordinates = coordinates_array[y]
-    print("point \#", y+1, "coordinates:", both_coordinates)
+    for z in range(0, y):
+        GPIO.output(8, GPIO.HIGH)
+        GPIO.output(10, GPIO.HIGH)
+        GPIO.output(12, GPIO.HIGH)
+        time.sleep(1)
+        GPIO.output(8, GPIO.LOW)
+        GPIO.output(10, GPIO.LOW)
+        GPIO.output(12, GPIO.LOW)
+        time.sleep(.5)
+
     time.sleep(3)
-    print("!\n!\n!\n!\n!\n")
     #Switched these
     lat1 = float(both_coordinates[1])
     long1 = float(both_coordinates[0])
     while atTarget((cur_lat, cur_long), (lat1, long1)) == False:
-        x = threading.Thread(target= steering, args = ())
+        x = threading.Thread(target = steering, args = ())
         x.start()
-        
-        #print("waypoint:", lat1, long1, " current position:", cur_lat, cur_long)
-        
-        
-        '''for event1 in pygame.event.get():
-            if event1.key == pygame.K_p: # replace the 'p' to whatever key you wanted to be pressed
-                 print("You pressed p!") #Do what you want to here
-            if event1.type == pygame.locals.QUIT:
-                 pygame.quit()
-                 sys.exit()'''
         
         goForwards()
         time.sleep(.1)
         stop()
         time.sleep(.2)
                   
+        
+        DISPLAYSURF.fill(BLACK)
+        
+        for event in pygame.event.get():
+            if event.type == QUIT: 
+                    pygame.quit()
+                    sys.exit
+        
+        pygame.draw.line(DISPLAYSURF, RED, (WIDTHMIDPOINT, 0), (WIDTHMIDPOINT, SCREENHEIGHT), 3)
+        pygame.draw.line(DISPLAYSURF, RED, (0, HEIGHTMIDPOINT), (WIDTHMIDPOINT, HEIGHTMIDPOINT), 3)       
+        
+        writeText("Variables: ", (WIDTHMIDPOINT + 10, 5))
+        
+        writeText("GPS ", (WIDTHMIDPOINT + 20, 25))
+        writeText("Going to waypoint: " + str(y + 1), (WIDTHMIDPOINT + 20, 65))
+        writeText("Waypoint Latitude: " + str(lat1) + ", Waypoint Longitude" + str(long1), (WIDTHMIDPOINT + 20, 85))
+        writeText("Latitude: " + str(cur_lat) + ", Longitude: " + str(cur_long), (WIDTHMIDPOINT + 20, 45))
+        #writeText("Latitude Tolerance: " + str(tolerancelat) + ", Longitude Tolerance: " + str(tolerancelong), (WIDTHMIDPOINT + 20, 105))
+        writeText("Latitude Tolerance: .00003, Longitude Tolerance: .00003", (WIDTHMIDPOINT + 20, 105))
+        writeText("Lat from Waypoint: " + str(round(cur_lat - lat1, 8)), (WIDTHMIDPOINT + 20, 125))
+        writeText("Long from Waypoint: " + str(round(cur_long - long1, 8)), (WIDTHMIDPOINT + 20, 145))
+        writeText("AtTarget ==  " + str(atTarget((cur_lat, cur_long), (lat1, long1))), (WIDTHMIDPOINT + 20, 165))
+        
+        writeText("Compass: ", (WIDTHMIDPOINT + 20, 205))
+        writeText("Current Heading: " + str(round(heading, 1)), (WIDTHMIDPOINT + 20, 225))
+        writeText("Desired Heading: " + str(round(projected_bearing, 1)), (WIDTHMIDPOINT + 20, 245))
+        writeText("Necessary Change: " + str(round(necessary_change, 1)), (WIDTHMIDPOINT + 20, 265))
+        writeText("Range of Straight: -5 < x < 5", (WIDTHMIDPOINT + 20, 285))
+        
+        writeText("Car: ", (WIDTHMIDPOINT + 20, 325))
+        writeText("Turning Position: " + position, (WIDTHMIDPOINT + 20, 345))
+        #writeText("Forwards Interval: " + position, (WIDTHMIDPOINT + 20, 345))
+        #writeText("Turning Position: " + position, (WIDTHMIDPOINT + 20, 365))
+    
+        pygame.display.update()        
+
                   
                   
 
